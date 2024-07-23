c6d8ec0854023bc7c7c557ba7d660e854eb58252
julio <julito2302mc@gmail.com>
2024-07-23 13:07:15 +0200

    refactor(router): landing page is now a route
    also generates a routes.enum for handling routes there

---
+++ src/app/shared/types/routes.enum.ts
@@ -0,0 +1,11 @@
+export enum CustomRoutes {
+  LANDING = 'landing',
+  HOME = 'inicio',
+  SIGN_IN = 'acceso',
+  SIGN_UP = 'registrarse',
+  FRIDGE = 'frigorifico',
+  SETTINGS = 'ajustes',
+  RECIPES = 'recetas',
+  RECIPES_GENERATOR = 'generador',
+  CONTACT = 'contactanos',
+}
