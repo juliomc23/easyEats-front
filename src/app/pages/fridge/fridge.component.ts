208c3f5849f822df9dc88196351e0a0cfcf2364f
julio <julito2302mc@gmail.com>
2024-07-29 15:53:01 +0200

    feat(view): add fridge service

--- src/app/pages/fridge/fridge.component.ts
+++ src/app/pages/fridge/fridge.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { LayoutComponent } from '../../shared/components/layout/layout.component';
 import { IngredientComponent } from './components/ingredient/ingredient.component';
-import { Ingredient } from './interfaces/ingredient.interface';
+import { FridgeService } from './services/fridge.service';
 
 @Component({
   selector: 'app-fridge',
@@ -11,15 +11,9 @@ import { Ingredient } from './interfaces/ingredient.interface';
   styleUrl: './fridge.component.scss',
 })
 export class FridgeComponent {
-  public ingredientsInFridge: Ingredient[] = [
-    { id: 1, name: 'Huevos', qty: { value: 12, unit: 'ud' } },
-    { id: 2, name: 'Leche', qty: { value: 12, unit: 'gr' } },
-    { id: 3, name: 'Carne', qty: { value: 12, unit: 'gr' } },
-    { id: 4, name: 'Pollo', qty: { value: 12, unit: 'gr' } },
-    { id: 5, name: 'Jamón', qty: { value: 12, unit: 'gr' } },
-    { id: 6, name: 'Queso', qty: { value: 12, unit: 'gr' } },
-    { id: 7, name: 'Pescado', qty: { value: 12, unit: 'gr' } },
-  ];
+  private fridgeService: FridgeService = inject(FridgeService);
+
+  fridgeIngredients$ = this.fridgeService.getFridgeIngredients();
 
   showAddIngredientSection = false;
 
