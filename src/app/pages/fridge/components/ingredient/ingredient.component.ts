ccb22e0fd19e253280749a55f240786aba4e8b15
julio <julito2302mc@gmail.com>
2024-07-29 15:52:25 +0200

    feat(component): add logic for edit ingredient

--- src/app/pages/fridge/components/ingredient/ingredient.component.ts
+++ src/app/pages/fridge/components/ingredient/ingredient.component.ts
@@ -1,13 +1,55 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { Ingredient } from '../../interfaces/ingredient.interface';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FridgeService } from '../../services/fridge.service';
 
 @Component({
   selector: 'app-ingredient',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './ingredient.component.html',
   styleUrl: './ingredient.component.scss',
 })
 export class IngredientComponent {
   @Input({ required: true }) ingredient!: Ingredient;
+
+  private fridgeService: FridgeService = inject(FridgeService);
+
+  isEditingFood: boolean = false;
+
+  ingrientFormGroup = new FormGroup({
+    name: new FormControl(),
+    qty: new FormGroup({
+      value: new FormControl(),
+      unit: new FormControl(),
+    }),
+  });
+
+  editFood() {
+    this.isEditingFood = true;
+    this.ingrientFormGroup.patchValue({
+      name: this.ingredient.name,
+      qty: {
+        value: this.ingredient.qty.value,
+        unit: this.ingredient.qty.unit,
+      },
+    });
+  }
+
+  cancelEditingFood(ingredientId: number) {
+    this.isEditingFood = false;
+  }
+
+  saveEditedFood(ingredient: Ingredient) {
+    this.isEditingFood = false;
+
+    const { name, qty } = this.ingrientFormGroup.value;
+
+    if (!name || !qty) return;
+
+    this.fridgeService.modifyIngredient(ingredient.id, {
+      name,
+      qty: { value: qty.value, unit: qty.unit },
+    });
+  }
 }
